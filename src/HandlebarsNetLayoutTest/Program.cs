76037a9dd1fcb404b75081dfd6d85b41dc58f55f
Jon Sagara <jon@sagarasoftware.com>
2017-05-23 10:32:52 -0700

    Initial commit.

---
+++ src/HandlebarsNetLayoutTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HandlebarsDotNet;
+
+namespace HandlebarsNetLayoutTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate handlebars with custom configuration.
+            var handlebars = Handlebars.Create(new HandlebarsConfiguration
+            {
+                FileSystem = new DiskFileSystem()
+            });
+
+            // Get the render function for our FileReady view.
+            var fileReadyRenderer = handlebars.CompileView("Views/EmailTemplates/FileReady.hbs");
+
+            // Render the view.
+            var html = fileReadyRenderer(new
+            {
+                SiteUrl = "http://dev.example.com",
+                FileName = "SomeFile.pdf"
+            });
+
+            Console.WriteLine(html);
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
